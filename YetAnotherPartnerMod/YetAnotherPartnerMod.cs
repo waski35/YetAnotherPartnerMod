ac7192423b361c673d7ce20c8494320864507a78
MarcinKałat <marcin.kalat@hotmail.com>
2016-02-09 03:50:09 +0100

    fixed path

--- YetAnotherPartnerMod/YetAnotherPartnerMod.cs
+++ YetAnotherPartnerMod/YetAnotherPartnerMod.cs
@@ -858,7 +858,7 @@ namespace YetAnotherPartnerMod
             {
                 return false;
             }
-            FileVersionInfo myFileVersionInfo2 = FileVersionInfo.GetVersionInfo("\\Plugins\\LSPD First Response.dll");
+            FileVersionInfo myFileVersionInfo2 = FileVersionInfo.GetVersionInfo("Plugins\\LSPD First Response.dll");
             if (myFileVersionInfo2.FileMajorPart >= 0)
             {
                 if (myFileVersionInfo2.FileMinorPart >= 3)
